2385402b238d38edc660bbd96cedb2c12be63e1a
Abdelaziz Soliman <abdelazizmalfy@gmail.com>
2021-09-29 09:24:16 +0200

    added push method to add to the beginnig of the stack

--- stack/stack.js
+++ stack/stack.js
@@ -15,4 +15,28 @@ class Stack {
         this.last = null
         this.length = 0
     }
+
+    // method to add to the beginning of the list
+    push(val) {
+        const newNode = new Node(val)
+
+        if (this.length === 0) {
+            this.first = newNode
+            this.last = newNode
+        } else {
+            const current = this.first
+            this.first = newNode
+            newNode.next = current
+        }
+
+        this.length++
+        return this.length
+    }
 }
+
+const stack = new Stack()
+
+console.log(stack.push(1))
+console.log(stack.push(2))
+console.log(stack.push(3))
+console.log(stack)
