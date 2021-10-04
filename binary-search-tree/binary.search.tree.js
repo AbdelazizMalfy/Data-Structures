b5da1503f3b0fe147223ce2deb2d045ee07d6193
Abdelaziz Soliman <abdelazizmalfy@gmail.com>
2021-10-04 17:15:23 +0200

    add insert method for binary search tree

--- binary-search-tree/binary.search.tree.js
+++ binary-search-tree/binary.search.tree.js
@@ -10,6 +10,40 @@ class BinarySearchTree {
     constructor() {
         this.root = null;
     }
+
+    // a method to insert node at the right position
+    insert(val) {
+        const newNode = new Node(val)
+
+        if(!this.root) {
+            this.root = newNode
+            return this
+        }
+
+        let currentNode = this.root
+
+        while(true) {
+            if(val === currentNode.val) {
+                return undefined
+            }
+
+            if(val < currentNode.val) {
+                if(currentNode.left) {
+                    currentNode = currentNode.left
+                } else {
+                    currentNode.left = newNode
+                    return this
+                }
+            } else if(val > currentNode.val) {
+                if(currentNode.right) {
+                    currentNode = currentNode.right
+                } else {
+                    currentNode.right = newNode
+                    return this
+                }
+            } 
+        }
+    }
 }
 
 const tree = new BinarySearchTree();
